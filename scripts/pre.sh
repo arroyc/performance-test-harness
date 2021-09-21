b514e45708bebfa3e181b0b144f09af75c0c4bda
Varad Meru <vameru@microsoft.com>
2021-09-21 06:57:20 +0000

    Updated pre.sh

--- scripts/pre.sh
+++ scripts/pre.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
+sudo sh -c "apt-get install --yes jq"
+
 for s in $(cat /home/adminuser/data/testconfig.json | jq '.test_env_vars' | jq -r -c "to_entries|.[]"); do
     key=$(echo $s | jq -r '.key')
     value=$(echo $s | jq -r '.value')
@@ -8,7 +10,6 @@ done
 sudo sh -c "mv /home/adminuser/data/test_env_var_exports.sh /etc/profile.d/test_env_var_exports.sh"
 sudo sh -c "chown root:root /etc/profile.d/test_env_var_exports.sh"
 sudo sh -c "chmod 644 /etc/profile.d/test_env_var_exports.sh"
-sudo sh -c "apt-get install --yes jq"
 
 # pre.sh must return an exit code to signal success or failure to the pipelines
 exit_code=$?
